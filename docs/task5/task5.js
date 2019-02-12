931ab03e26766328f65cbddf51c00ed017f967ac
Pavel Kvasov <33542521+pxpn@users.noreply.github.com>
2019-02-12 13:51:41 +0300

    1

--- docs/task5/task5.js
+++ docs/task5/task5.js
@@ -1,4 +1,22 @@
-function elements(){
-
+var elements = function(quantity){
+    var element = [];
     
+    for (var i = 0; i < quantity; i++) {
+        element [i] = prompt ('Введи элемент')
+    }
+    var minEl = element[0];
+    var maxEl = element[0];
+    var sumEl = 0;
+    for (var i = 0; i < quantity; i++) {
+        if (element[i] < minEl){
+            minEl = element[i];
+        }
+        if (element[i] > maxEl){
+            maxEl = element[i];
+        }
+        sumEl += +element[i];
+    }
+    return alert('Максимальное - ' + maxEl + '. Минимальное - ' + minEl + '. Сумма = ' + sumEl)
+
 }
+elements(prompt ('Введи кол-во элементов'))
