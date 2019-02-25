0554bd0ad705e66c908c2ce16ae18f89647cf7dd
Pavel Kvasov <33542521+pxpn@users.noreply.github.com>
2019-02-25 22:21:30 +0300

    1

--- docs/task3/task3-3.js
+++ docs/task3/task3-3.js
@@ -1,3 +1,10 @@
 function task33(){
     var chislo = prompt('Введите число')
+    var chisloArr = chislo.split('')
+    var reverse = []
+    for (var i = chisloArr.length-1; i>=0 ; i--) {
+        reverse.push(chisloArr[i])
+
+    }
+    alert(reverse.join(''));
 }
